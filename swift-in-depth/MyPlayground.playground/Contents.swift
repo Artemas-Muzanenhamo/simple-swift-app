746316bb9762a861d0bb681f610876804ebbbe45
Artemas-Muzanenhamo <artemasseng@gmail.com>
2018-04-18 12:54:21 +0100

    add Operators in playground

--- swift-in-depth/MyPlayground.playground/Contents.swift
+++ swift-in-depth/MyPlayground.playground/Contents.swift
@@ -40,3 +40,22 @@ print("Hi there"); print("Another Print")
 let st = "world"
 print("hello \(st)")
 
+// OPERATORS
+123.5.truncatingRemainder(dividingBy: 123)
+
+let range = 1...3 // represent range from 1 to and including 3
+let anotherRange = 1..<3 // represent range from 1 to and excluding 3
+1+1 // addition
+2*3 // multiplication
+10/5 //division
+4-2 // subtraction
+1<2 // conditional equality
+2>1 // conditional equality
+1>2 // conditional equality
+4==4 // equality
+var i = 32 // assignment
+i>>1 // right shift operator
+i<<1 // left-shift operator
+var a = 20
+var b = 20
+let isTrue = a == b // Logical or Operator
